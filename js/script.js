1178c57a94724c0132d98e038b7eb3f10acdb7d1
matsta25 <matim2112@gmail.com>
2018-04-10 22:01:37 +0200

    AIO

---
+++ js/script.js
@@ -0,0 +1,34 @@
+//side nav
+const sideNav = document.querySelector('.sidenav');
+M.Sidenav.init(sideNav, {});
+
+//slider
+const slider = document.querySelector('.slider');
+M.Slider.init(slider,{
+    indicators: false,
+    height: 500,
+    transition: 500,
+    interval: 6000
+});
+
+// autocomplete
+const ac = document.querySelector('.autocomplete');
+M.Autocomplete.init(ac,{
+    data:{
+        "Aruba": null,
+        "Cancun Mexico": null,
+        "Hawaii": null,
+        "Florida": null,
+        "California": null,
+        "Jamacia": null,
+        "Europe": null
+    }
+});
+
+// material boxed = gallery
+const mb = document.querySelectorAll('.materialboxed');
+M.Materialbox.init(mb, {});
+
+// scrollspy
+const ss = document.querySelectorAll('.scrollspy');
+M.Scrollspy.init(ss,{});
